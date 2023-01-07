0c6a8b93c3e9ac400ce1a2faddbb9dda54c05df5
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-07 20:43:40 +0100

    add js

--- sql/G5/revisionPHP/index.php
+++ sql/G5/revisionPHP/index.php
@@ -3,21 +3,21 @@
 <head>
     <title>Formulaire admission</title>
     <link rel="stylesheet" type="text/css" href="./public/css/index.css">
-    <script src="./public/js/index.js"></script>
 </head>
 <body>
-<form id="form" method="POST" action="traitement.php">
+<form method="POST" action="traitement.php" onsubmit="formSubmit()">
     <label>Nom</label>
     <input type="text" name="nom">
     <label>Prénom</label>
     <input type="text" name="prenom">
     <label>Date de naissance</label>
-    <input type="date"  id="date" name="dateNaissance" >
+    <input type="date"  id="date" name="dateNaissance">
     <label>e-mail</label>
     <input id="email1" type="text" name="email">
     <label>Confirmez email</label>
     <input id="email2" type="text" name="emailConfirmation">
     <input type="submit" value="Soumettez votre candidature">
 </form>
+<script src="./public/js/index.js"></script>
 </body>
 </html>
--- sql/G5/revisionPHP/public/js/index.js
+++ sql/G5/revisionPHP/public/js/index.js
@@ -1,4 +1,27 @@
-const myForm = document.getElementById('form');
-myForm.onsubmit = function (){
-    alert()
+function formSubmit (){
+    confirm("Etes-vous sûr de soumettre votre candidature ?")
+}
+let date = document.getElementById('date');
+date.addEventListener('focusout', (event) => {
+
+    let dateOfBirth = new Date(date.value);
+    let age = new Date().getFullYear() - dateOfBirth.getFullYear();
+    let month = new Date().getMonth() - dateOfBirth.getMonth();
+
+    if (month < 0 || (month === 0 && new Date().getDate() < dateOfBirth.getDate())) {
+        age--;
+    }
+
+    if (age < 18 && age >= 0) {
+        alert('Vous êtes trop jeune');
+        date.style.color = 'red';
+    }
+    if (age < 0){
+        alert('Vous devez être né pour utiliser cette application');
+        date.style.color = 'red';
+    }
+    else date.style.color = 'green';
+});
+function ageChecker(date){
+
 }
