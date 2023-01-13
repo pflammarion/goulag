511fcd4e658bdceb9c0be93107ebf9b36852bfa2
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-13 09:37:37 +0100

    add jquery

--- php-mvc-simple/view/form.php
+++ php-mvc-simple/view/form.php
@@ -2,11 +2,15 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
     <title>MVC PHP</title>
+    <link rel="stylesheet" href="../public/style.css">
+    <script src="../public/jquery-3.6.1.min.js"></script>
 </head>
 <body>
 <?php
 echo $message;
 ?>
+<script src="../public/index.js"></script>
 </body>
 </html>
