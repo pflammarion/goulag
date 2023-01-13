3f1661ea4f17a071b7ddac8ff0c7f0befafd4ae8
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-13 15:28:11 +0100

    bdd connection

--- eval-php/model/Personne.php
+++ eval-php/model/Personne.php
@@ -4,6 +4,23 @@ class Personne {
 
     public function insertScore(string $nom, string $prenom, string $email, int $quiz): bool
     {
+        $db = $this->databaseConnection();
+        $sql = "INSERT INTO personne (nom, prenom, email, quiz, date) VALUES (:nom, :prenom, :email, :quiz, CURRENT_TIMESTAMP()) ";
+        $query = $db-> prepare($sql);
+        $query->execute(
+            array(
+                'nom'=>$nom,
+                'prenom'=>$prenom,
+                'email'=>$email,
+                "quiz"=> $quiz,
+            )
+        );
+        return true;
+    }
+
+    private function databaseConnection(): PDO
+    {
+        //database connection cred
         $db_name =  'quiz';
         $db_user = 'root';
         $db_pass = '';
@@ -18,19 +35,7 @@ class Personne {
             echo 'Database connection error.'.$e->getMessage();
             exit();
         }
-
-
-        $sql = "INSERT INTO personne (nom, prenom, email, quiz, date) VALUES (:nom, :prenom, :email, :quiz, CURRENT_TIMESTAMP()) ";
-        $query = $db-> prepare($sql);
-        $query->execute(
-            array(
-                'nom'=>$nom,
-                'prenom'=>$prenom,
-                'email'=>$email,
-                "quiz"=> $quiz,
-            )
-        );
-        return true;
+        return $db;
     }
 
 }
